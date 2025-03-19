e9a5673ad88a87bf7cb7c81b68c180948e8e85a3
SaiSreenivas_nehask_Datascitist <sreenivassaibks@gmail.com>
2025-03-19 17:30:00 +0530

    added configYaml

--- demo.py
+++ demo.py
@@ -1,3 +1,16 @@
 from ml_pipline.logger import logger
+from ml_pipline.pipline.stage1_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("welcome to the ML_GENAI_MLOPS")
+# logger.info("welcome to the ML_GENAI_MLOPS")
+
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<")
+    data_ingestion= DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>>> stage {STAGE_NAME} completed >>>>>>")
+except Exception as e:
+    logger.exception(e)
+    raise e
--- ml_pipline/constants/__init__.py
+++ ml_pipline/constants/__init__.py
@@ -1,6 +1,18 @@
 from pathlib import Path
 
+#D:\Mlops_eur\ml_pipeline\config\config.yaml
+#ml_pipeline\ml_pipline\config_u\config.yaml
 
-CONFIG_FILE_PATH = Path("config/config.yaml")
+#D:\Mlops_eur\ml_pipeline\config
+#ml_pipeline\config\config.yaml
+#D:\Mlops_eur\ml_pipeline\ml_pipline\config_u\config.yaml
+#CONFIG_FILE_PATH = Path(r"ml_pipeline\ml_pipeline\config_u\config.yaml")
+#D:\Mlops_eur\ml_pipeline\ml_pipline\constants\__init__.py
+#ml_pipeline\config\config.yaml
+
+CONFIG_FILE_PATH = Path(r"ml_pipeline/config/config.yaml")
+print(CONFIG_FILE_PATH,"config location")
 PARAMS_FILE_PATH = Path("config/parmas.yaml")
 SCHEMA_FILE_PATH = Path("config/schema.yaml")
+# import os
+# print("Current Working Directory:", os.getcwd())
